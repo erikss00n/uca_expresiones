3891f0b3e84499e7502f273d3dd13f4f1f650743
erikss00n <erikolivergrz@outlook.com>
2026-09-25 23:24:00 +0000

    Paso 3: prueba unitaria de calcular_dano_promedio

--- tipos_operandos_rust/src/main.rs
+++ tipos_operandos_rust/src/main.rs
@@ -3,12 +3,18 @@ fn calcular_oleadas(derrotados: i32, por_oleada: i32) -> i32 {
 }
 fn calcular_dano_critico(dano_base: i32, multiplicador: f64) -> f64 {
     dano_base as f64 * multiplicador
+    
+}
+fn calcular_dano_promedio(dano_base: i32, por_oleada: i32) -> f64 {
+    dano_base as f64 / por_oleada as f64
 }
 fn main() {
     let dano_critico = calcular_dano_critico(50, 1.5);
     println!("Daño crítico: {}", dano_critico);
     let oleadas = calcular_oleadas(7, 2);
     println!("Oleadas: {}", oleadas);
+    let dano_promedio = calcular_dano_promedio(50, 2);
+    println!("Daño promedio: {}", dano_promedio);
 }
 
 #[cfg(test)]
@@ -21,5 +27,9 @@ mod tests {
     #[test]
     fn test_calcular_oleadas() {
         assert_eq!(calcular_oleadas(7, 2), 3);
+    }
+        #[test]
+    fn test_calcular_dano_promedio() {
+        assert_eq!(calcular_dano_promedio(50, 2), 25.0);
     }
 }

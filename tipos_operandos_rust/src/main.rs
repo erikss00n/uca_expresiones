370516be100cf36f821b3abca2d6c1f411c23881
erikss00n <erikolivergrz@outlook.com>
2026-09-25 23:18:07 +0000

    Paso 1: prueba unitaria de calcular_oleadas

--- tipos_operandos_rust/src/main.rs
+++ tipos_operandos_rust/src/main.rs
@@ -1,3 +1,18 @@
+fn calcular_oleadas(derrotados: i32, por_oleada: i32) -> i32 {
+    derrotados / por_oleada
+}
+
 fn main() {
-    println!("Hello, world!");
+    let oleadas = calcular_oleadas(7, 2);
+    println!("Oleadas: {}", oleadas);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_calcular_oleadas() {
+        assert_eq!(calcular_oleadas(7, 2), 3);
+    }
 }
